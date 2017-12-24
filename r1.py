4e60bdfe08ee3312cd461fbb08487a6be8d1f491
Allen <learn.to.code.tw2@gmail.com>
2017-12-25 07:25:40 +0800

    fix empty lines

--- r1.py
+++ r1.py
@@ -1,10 +1,12 @@
 
+
 def read_file(filename):
 	lines = []
 	with open(filename, 'r', encoding='utf-8-sig') as f:
 		for line in f:
 			lines.append(line.strip())
 	return lines
+
 
 def convert(lines):
 	new = []
